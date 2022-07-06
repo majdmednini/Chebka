ec3fa9a946b1ef7312a7ba2be07bdff9445c4295
stellarAlien <saifgaida@gmail.com>
2022-07-06 18:22:38 +0100

    comments for views and why certain things were chosen

--- NewsApp/views.py
+++ NewsApp/views.py
@@ -3,11 +3,14 @@ from newsapi import NewsApiClient
 from django.views.decorators.cache import cache_page
 from django.views.decorators.http import require_http_methods
 
+#permit only get methods to prevent http header attacks 
 @require_http_methods(["GET"])
+#cache the page for a day in seconds
 @cache_page(60*60*24)
 def index(request, src):
     newsapi = NewsApiClient(api_key="d82ee41695e140d78c1726d37ae3a703")
     topheadlines = newsapi.get_top_headlines(source = src)
+    #get only the artciles without the other metadata
     articles = topheadlines['articles']
 
     desc = []
@@ -23,7 +26,10 @@ def index(request, src):
         img.append(article['urlToImage'])
         url.append(article['url'])
 
-
+    #make a list of lists and this is the tighr syntax to  do it
+    #using zip method for faster rendering time
+    #might use itertools for next features and zip two lists
+    #that needs a differnet syntax
     mylist = zip(news, desc, img, url)
 
 
